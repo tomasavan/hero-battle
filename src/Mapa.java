9a4f09b7355547646a0c8fc86db9e9033f7431b5
Tomas Savan <savan.tomas@gmail.com>
2019-11-01 20:59:41 -0300

    Se implementa la clase list en Mapa

--- src/Mapa.java
+++ src/Mapa.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mapa {
-    protected String morgos, steed, estructuras, creeps;
+
+    protected String morgos, steed, estructura, creep;
+
+    public Mapa(String morgos, String steed,
+                String estructuras, String creeps) {
+
+        List<Creep> creepList = new ArrayList<Creep>();
+        creepList.add(new Creep("Creep Melee", 20,
+                1, 330));
+        creepList.add(new Creep("Creep a Distancia", 33,
+                1, 300));
+
+        List<Estructura> estructuraList = new ArrayList<Estructura>();
+
+        estructuraList.add(new Estructura("Torre", 40,
+                4, 900, 5));
+        estructuraList.add(new Estructura("Barraca", 0,
+                10, 1300, 8));
 
 
-
-
+    }
 }
